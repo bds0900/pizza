6b07625431eed946635c3334239f0562db25ab0b
bds0900 <Dbeak9336@conestogac.on.ca>
2020-11-18 14:33:19 -0500

    add blazor client

---
+++ Client/Models/PizzaInfo.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class PizzaInfo
+    {
+        public List<Size> Size { get; set; }
+        public List<Entities.Type> Type { get; set; }
+        public List<Topping> Topping { get; set; }
+    }
+}
