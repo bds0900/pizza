ecf336f0c5e995560010f58754f5b9094d589871
bds0900 <Dbeak9336@conestogac.on.ca>
2020-11-19 11:09:36 -0500

    add side menu

---
+++ Client/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public int TypeId { get; set; }
+        public int SizeId { get; set; }
+        public int[] ToppingId { get; set; }
+        public float Subtotal { get; set; }
+        public float Tax { get; set; }
+        public float Total { get; set; }
+    }
+}
---
+++ Client/Store/StateContainer.cs
@@ -0,0 +1,61 @@
+﻿using Client.Models;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Store
+{
+    public class StateContainer
+    {
+        public string Property { get; set; } = "Initial value from StateContainer";
+
+        public event Action OnChange;
+        public void SetProperty(string value)
+        {
+            Property = value;
+            NotifyStateChanged();
+        }
+
+        public float SubTotal { get; set; } = 0;
+        public float Tax { get; set; } = 0;
+        public float Total { get; set; } = 0;
+        public List<Item> Items { get; set; } = new List<Item>();
+        public PizzaInfo PizzaInfo { get; set; } = null;
+        public List<Side> Sides { get; set; } = null;
+        public void SetSubTotal(float subtotal)
+        {
+            SubTotal = subtotal;
+            NotifyStateChanged();
+        }
+        public void SetTax(float tax)
+        {
+            Tax = tax;
+            NotifyStateChanged();
+        }
+        public void SetTotal(float total)
+        {
+            Total = total;
+            NotifyStateChanged();
+        }
+        public void SetItems(List<Item> items)
+        {
+            Items = items;
+            NotifyStateChanged();
+        }
+        public void SetPizzaInfo(PizzaInfo pizzainfo)
+        {
+            PizzaInfo = pizzainfo;
+            NotifyStateChanged();
+        }
+        public void SetSide(List<Side> side)
+        {
+            Sides = side;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
